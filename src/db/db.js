f4764b04bb9662e6cf2bb1ab73dd439e5b254842
Haroldo <hlbacellar@stefanini.com>
2021-06-18 16:19:33 -0300

    removing faunadb key

--- src/db/db.js
+++ src/db/db.js
@@ -5,7 +5,7 @@ const { Get, Create, Update, Index, Ref, Collection, Paginate, Documents, Map, L
 class DB {
     constructor() {
         this.client = new faunadb.Client({
-            secret: 'fnAEJtK5kbACAGUDTBq38Ycg0QhJSIsJITF9NH8Y'
+            secret: process.env.FAUNDABSECRET
         });
 
     }
